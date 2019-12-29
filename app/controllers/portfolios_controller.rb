ff754bc98927d85ded62d40402f8a80924cc5f63
Haggai Lerman <haguyle@gmail.com>
2019-12-28 21:08:55 -0500

    Implemented destroy action for protfolio freature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,6 +37,16 @@ class PortfoliosController < ApplicationController
 
       def show
           @portfolio_item = Portfolio.find(params[:id])
-          
+      end
+      def destroy
+         # Preform the lookup
+        @portfolio_item = Portfolio.find(params[:id])
+
+        # Destroy/delete the record
+        @portfolio_item.destroy
+        # Redirect
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: 'record was successfully remove.' }
+        end
       end
 end
