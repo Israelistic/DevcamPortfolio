11d7da74d738c3665c69e01363974b4c85b88c65
Haggai Lerman <haguyle@gmail.com>
2020-01-09 23:16:49 -0500

    Implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,8 @@
 class Skill < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :precent_utilized
-
+    after_initialize :set_defaults
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    end
 end
